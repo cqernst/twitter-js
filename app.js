f848e43fac43e2f7badc36fd1bc69c9a79234374
Christine Ernst <christineernst@Christines-MacBook-Air.local>
2018-03-06 16:21:59 -0500

    upate

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.listen(3000, () => {
 	console.log('server listening')
 })
 
+app.use((req, res, next) => {
+	console.log('doing this for every incoming requset');
+})
+
 app.get('/', (res, req) => {
 	req.send('Welcome!');
 })
+
+//what does res.json do?
